05d8784379340baae9c161971f45a3911d7c2d6a
cstazinski29 <cstazinski@gmail.com>
2022-04-11 20:03:45 -0400

    worked up until RR API 3.05

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantRaterAPI.Models;
+using System.Threading.Tasks;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _context;
+        public RestaurantController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostRestaurant([FromForm] RestaurantEdit model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Restaurants.Add(new Restaurant()
+            {
+                Name = model.Name,
+                Location = model.Location,
+            });
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRestaurants()
+        {
+            var restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetRestaurantById(int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+    }
+}
